78cd25fa9735239526464ce3d5a38963a5afd31a
Cruz Godar <cruz.godar@gmail.com>
2026-04-19 11:00:55 -0400

    Added inline code and escaped characters

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ const ohm = require("ohm-js");
 const grammar = String.raw`
 	plecta {
 		text = inline<end>*
-		
-		bold
-			= "**" ~"*" inline<"**">+ "**" ~"*"
-			| "__" ~"_" inline<"__">+ "__" ~"_"
-			
-		italic
-			= "*" ~"*" inline<"*">+ "*" ~"*"
-			| "_" ~"_" inline<"_">+ "_" ~"_"
-			
+
 		boldItalic
 			= "***" inline<"***">+ "***"
 			| "___" inline<"___">+ "___"
-		
-		inline<stop> = boldItalic | bold | italic | (~stop ~newline any)
-		
+
+		bold
+			= "**" ~"*" inline<"**">+ "**" ~"*"
+			| "__" ~"_" inline<"__">+ "__" ~"_"
+
+		italic
+			= "*" ~"*" inline<"*">+ "*" ~"*"
+			| "_" ~"_" inline<"_">+ "_" ~"_"
+
+		code = "\`" ~"\`" inline<"\`">+ "\`" ~"\`"
+
+		escape = "\\" ("@" | "*" | "_" | "\\")
+
+		inline<stop>
+			= boldItalic
+			| bold
+			| italic
+			| code
+			| escape
+			| (~stop ~newline any)
+
 		newline = "\r\n" | "\n" | "\r"
-		}
+	}
 `;
 
 const plecta = ohm.grammar(grammar);
