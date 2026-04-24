7eb680cf166aeefd53aac7b88153381dc1ec2480
Cruz Godar <cruz.godar@gmail.com>
2026-04-24 10:11:47 -0400

    Added code blocks

--- index.js
+++ index.js
@@ -1,59 +1,81 @@
 const ohm = require("ohm-js");
 
 const grammar = String.raw`
-	plecta {
-		document = (paragraph | twoOrMoreNewlines)+
-
-
-		
-		heading = spaceOrTab* "#" "#"? "#"? "#"? "#"? "#"? spaceOrTab+ inline<(newline | end)>+
-
-
+plecta {
+	document = chunk+
 	
-		paragraph = (inline<(doubleNewline | end)> | newline ~newline)+
-
-		boldItalic
-			= "***" inline<"***">+ "***"
-			| "___" inline<"___">+ "___"
-
-		bold
-			= "**" ~"*" inline<"**">+ "**" ~"*"
-			| "__" ~"_" inline<"__">+ "__" ~"_"
-
-		italic
-			= "*" ~"*" inline<"*">+ "*" ~"*"
-			| "_" ~"_" inline<"_">+ "_" ~"_"
-
-		link = "[" inline<"]">+ "]" "(" inlineRaw<")">+ ")" 
+	chunk
+		= heading
+		| codeBlock
+		| paragraph
+		| twoOrMoreNewlines
+	
+	
+	
+	heading = spaceOrTab* "#" "#"? "#"? "#"? "#"? "#"? spaceOrTab+ inline<(newline | end)>+
+	
+	
+	
+	codeBlock
+		= spaceOrTab* "\`\`\`" spaceOrTab* alnum* spaceOrTab* newline
+		codeBlockBody
+		newline spaceOrTab* "\`\`\`" &(newline | end)
 		
-		code = "\`" ~"\`" inlineRaw<"\`">+ "\`" ~"\`"
+	codeBlockBody = (~(newline spaceOrTab* "\`\`\`" (newline | end)) any)*
+	
+	
+	
+	paragraph = (inline<(doubleNewline | end)> | newline ~newline)+
 
-		math = "$" ~"$" inlineRaw<"$">+ "$" ~"$"
-		displayMath = "$$" inlineRaw<"$$">+ "$$"
+	boldItalic
+		= "***" inline<"***">+ "***"
+		| "___" inline<"___">+ "___"
 
-		escape = "\\" ("@" | "*" | "_" | "$" | "\`" | "\\")
+	bold
+		= "**" ~"*" inline<"**">+ "**" ~"*"
+		| "__" ~"_" inline<"__">+ "__" ~"_"
 
-		inline<stop>
-			= boldItalic
-			| bold
-			| italic
-			| code
-			| math
-			| displayMath
-			| link
-			| escape
-			| (~stop ~newline any)
+	italic
+		= "*" ~"*" inline<"*">+ "*" ~"*"
+		| "_" ~"_" inline<"_">+ "_" ~"_"
+	
+	link = "[" inline<"]">+ "]" "(" inlineRaw<")">+ ")" 
 
-		// Used for delimeters that aren't folding
-		inlineRaw<stop>
-			= ("\\" ("\\" | stop) | ~stop ~newline any)
+	// Code and math are non-folding
+	code = "\`" ~"\`" inlineRaw<"\`">+ "\`" ~"\`"
 
-		newline = "\r\n" | "\n" | "\r"
-		doubleNewline = newline newline
-		twoOrMoreNewlines = doubleNewline newline*
+	inlineMath = "$" ~"$" inlineRaw<"$">+ "$" ~"$"
+	inlineDisplayMath = "$$" inlineRaw<"$$">+ "$$"
 
-		spaceOrTab = " " | "\t"
-	}
+	escape = "\\" ("@" | "*" | "_" | "$" | "\`" | "\\")
+
+	inline<stop>
+		= boldItalic
+		| bold
+		| italic
+		| code
+		| inlineMath
+		| inlineDisplayMath
+		| link
+		| escape
+		| (~stop ~newline any)
+		
+	escapeRaw<stop> = "\\" ("\\" | stop)
+
+	inlineRaw<stop>
+		= escapeRaw<stop>
+		| (~stop ~newline any)
+		
+	blockInteriorRaw<stop>
+		= escapeRaw<stop>
+		| (~(newline spaceOrTab* stop) any)
+
+	newline = "\r\n" | "\n" | "\r"
+	doubleNewline = newline newline
+	twoOrMoreNewlines = doubleNewline newline*
+	
+	spaceOrTab = " " | "\t"
+}
 `;
 
 const plecta = ohm.grammar(grammar);
